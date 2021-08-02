23a33586a055cccb2573abce7a76865dadb05276
Pancham <panchamb63@gmail.com>
2021-08-02 17:51:34 +0530

    Made an api based plugin

--- src/components/api/apiComponent.js
+++ src/components/api/apiComponent.js
@@ -6,17 +6,22 @@ const ApiComponent = (props) => {
     const { link } = props?.data
     const [state, setState] = useState([])
     useEffect(() => {
-        axios.get("http://localhost:8000/").then((res) => {
+        axios.get(`${link}`).then((res) => {
             setState(res.data)
         }).catch((err) => {
             console.log(err)
         })
-    }, [])
+    }, [link])
     return (
-        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
             {state.map((prop) => {
                 return (
-                    <div key={prop.id} style={{ color: "red", border: "2px solid black", borderRadius: "1rem", padding: "10px 20px" }}>{prop.name}</div>
+                    <div onClick={() => console.log(link)} key={prop.id} style={{ border: "1.2px solid black", borderRadius: "0.2rem", padding: "10px 20px", background: "white", margin: "0.5rem", display: "inline-block", width: "24rem" }}>
+                        <div style={{ fontSize: "1.5rem", padding: "10px", margin: "10px", display: "inline-block" }}>{prop.name}</div>
+                        <div style={{ margin: "10px", display: "inline-block" }}>
+                            {prop.work}
+                        </div>
+                    </div>
                 )
             })}
         </div>
